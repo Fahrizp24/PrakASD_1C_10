3d3555f92e5ae5cbb89cb33a29c8cad8809e7281
Fahrizp24 <Fpradian24@gmail.com>
2024-02-15 16:31:42 +0700

    P1_Tugas 2_1C_10

---
+++ P1/Tugas2.java
@@ -0,0 +1,55 @@
+package P1;
+import java.util.Scanner;
+public class Tugas2 {
+    public static void main(String[] args) {
+        int s,v,t;
+        int menu=menu();
+        Scanner input10=new Scanner(System.in);
+        switch (menu) {
+            case 1:
+                System.out.print("Masukkan jarak (meter): ");
+                s=input10.nextInt();
+                System.out.print("Masukkan waktu (detik): ");
+                t=input10.nextInt();
+                kecepatan(s,t);
+                break;
+            case 2:
+                System.out.print("Masukkan kecepatan (meter/detik): ");
+                v=input10.nextInt();
+                System.out.print("MAsukkan waktu (detik): ");
+                t=input10.nextInt();
+                jarak(v, t);
+                break;
+            case 3:
+                System.out.print("MAsukkan jarak (meter): ");
+                s=input10.nextInt();
+                System.out.print("MAsukkan kecepatan (meter/detik): ");
+                v=input10.nextInt();
+                waktu(s, v);
+                break;
+            default:
+                break;
+        }
+
+    }
+    static int menu(){
+        System.out.println("Rumus apa yang ingin anda gunakan?");
+        System.out.println("1. Mencari Kecepatan\n2. Mencari Jarak\n3. Mencari Waktu");
+        Scanner input10 = new Scanner(System.in);
+        int menu=input10.nextInt();
+        return menu;
+    }
+    static void kecepatan(int s, int t){
+        int v = s/t;
+        System.out.println("Kecepatannya adalah "+v+" m/detik");
+    }
+    static void jarak(int v, int t){
+        int s=v*t;
+        System.out.println("Jaraknya adalah "+s+" meter");
+    }
+    static void waktu(int s, int v){
+        int t=s/v;
+        System.out.println("Waktunya adalah "+t+" detik");
+    }
+
+}
