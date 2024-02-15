237b9cd1c4ceaad639ed8decae3e32cba08849f7
Fahrizp24 <Fpradian24@gmail.com>
2024-02-15 12:02:02 +0700

    P1_Percobaan4_Pertanyaan2_1C_`10

--- P1/Fungsi.java
+++ P1/Fungsi.java
@@ -10,15 +10,21 @@ public class Fungsi {
             {5,7,12,9}};
         int harga[]={75000,50000,60000,10000};
         String bunga[]={"Aglonema","Keladi","Alocasia","Mawar"};
-        pendapatan(harga, stokbunga);
+        hitungStok(stokbunga, bunga);
     }
-    static void pendapatan(int harga[],int terjual[][]){
-        int pendapatanpercabang[]=new int[4];
-        for (int i = 0; i < pendapatanpercabang.length; i++) {
-            for (int j = 0; j < harga.length; j++) {
-                pendapatanpercabang[i]+=terjual[i][j]*harga[j];
-            }
-            System.out.println("Pendapatan dari cabang ke-"+(i+1)+" adalah: "+pendapatanpercabang[i]);
+    static void hitungStok(int stok[][],String bunga[]){
+        System.out.println("Cabang ke berapa yang ingin dirubah?");
+        Scanner input10 = new Scanner(System.in);
+        int cabang = input10.nextInt();
+        for (int i = 0; i < stok.length; i++) {
+            System.out.print("masukkan bunga "+bunga[i]+" yang mati: ");
+            int bungaMati=input10.nextInt();
+            stok[cabang-1][i]-=bungaMati;
+        }            
+        System.out.println("\nStok bunga pada cabang RoyalGarden 4 ");
+        for (int i = 0; i < bunga.length; i++) {
+            System.out.println("Stok bunga "+bunga[i]+": "+stok[cabang-1][i]);
         }
     }
+    
 }
