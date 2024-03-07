059aa80d1cc1b4ee5ac86c44f7d41ca45cf97de6
Fahrizp24 <Fpradian24@gmail.com>
2024-03-07 09:46:57 +0700

    P3_percobaan3_pertanyaan2_1C_10

---
+++ P3/ArrayBalok/Segitiga10.java
@@ -0,0 +1,10 @@
+package P3.ArrayBalok;
+public class Segitiga10 {
+    public int alas, tinggi;
+    double keliling;
+    
+    public Segitiga10(int a, int t){
+        alas = a;
+        tinggi=t;
+    }
+}
