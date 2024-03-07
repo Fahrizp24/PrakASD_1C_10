ed0b16ac867416010f973b3ad803facbec381a0a
Fahrizp24 <Fpradian24@gmail.com>
2024-03-07 08:42:46 +0700

    P3_Percobaan2_Pertanyaan2_1C_10

--- P3/AOBpersegiPanjang/ArrayOBjects10.java
+++ P3/AOBpersegiPanjang/ArrayOBjects10.java
@@ -18,6 +18,23 @@ public class ArrayOBjects10 {
             System.out.println("Persegi Panjang ke-"+i);
             System.out.println("Panjang: "+ppArray[i].panjang+", lebar: "+ppArray[i].lebar);
         }
+        PersegiPanjang10 [][] ppArray2D = new PersegiPanjang10[3][3];
+        for (int i = 0; i < ppArray2D.length; i++) {
+            for (int j = 0; j < ppArray2D[0].length; j++) {
+                ppArray2D[i][j]=new PersegiPanjang10();
+                System.out.println("Persegi Panjang Baris ke-"+i+", kolom ke-"+j);
+                System.out.print("Masukkan Panjang: ");
+                ppArray2D[i][j].panjang=input10.nextInt();
+                System.out.print("Masukkan Lebar: ");
+                ppArray2D[i][j].lebar=input10.nextInt();
+            }
+        }
+        for (int i = 0; i < ppArray.length; i++) {
+            for (int j = 0; j < ppArray2D.length; j++) {
+                System.out.println("Persegi Panjang Baris ke-"+i+", kolom ke-"+j);
+                System.out.println("Panjang: "+ppArray2D[i][j].panjang+", lebar: "+ppArray2D[i][j].lebar);
+            }
+        }
 
     }
 }
