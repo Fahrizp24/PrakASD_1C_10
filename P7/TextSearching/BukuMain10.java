d84708b47a3b520747a429fb768a50f224cf0f58
Fahrizp24 <Fpradian24@gmail.com>
2024-04-09 22:40:54 +0700

    P7_Percobaan2_Pertanyaan3_1C_10

--- P7/TextSearching/BukuMain10.java
+++ P7/TextSearching/BukuMain10.java
@@ -17,11 +17,11 @@ public class BukuMain10 {
         Buku10 m2= new Buku10(125,"Desain UI",2021,"Supriandi", 3);
         Buku10 m3 = new Buku10(126,"Web Programming",2022,"Pustaka Adi", 2);
         Buku10 m4 = new Buku10(127,"Etika Mahasiswa",2023,"Darmawan Adi", 2);
-        data.tambah(m);
         data.tambah(m1);
-        data.tambah(m2);
         data.tambah(m3);
+        data.tambah(m);
         data.tambah(m4);
+        data.tambah(m2);
 
         // System.out.println("==============================");
         // System.out.println("Data keseluruhan buku: ");
