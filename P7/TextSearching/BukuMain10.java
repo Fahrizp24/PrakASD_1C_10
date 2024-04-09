46de555c49b776bc675039a2b143e342a111b216
Fahrizp24 <Fpradian24@gmail.com>
2024-04-09 22:43:21 +0700

    P7_percobaan2_Pertanyaan4_1C_10

--- P7/TextSearching/BukuMain10.java
+++ P7/TextSearching/BukuMain10.java
@@ -11,17 +11,16 @@ public class BukuMain10 {
         int jumBuku=5;
 
         System.out.println("==============================");
-        System.out.println("Masukkan data buku secara urut dari kode buku terkecil :");
-        Buku10 m = new Buku10(111,"Algoritma",2019,"Wahyuni", 5);
-        Buku10 m1 = new Buku10(123,"Big Data",2020,"Susilo", 3);
-        Buku10 m2= new Buku10(125,"Desain UI",2021,"Supriandi", 3);
-        Buku10 m3 = new Buku10(126,"Web Programming",2022,"Pustaka Adi", 2);
-        Buku10 m4 = new Buku10(127,"Etika Mahasiswa",2023,"Darmawan Adi", 2);
-        data.tambah(m1);
-        data.tambah(m3);
+        Buku10 m = new Buku10(20215,"Algoritma",2019,"Wahyuni", 5);
+        Buku10 m1 = new Buku10(20214,"Big Data",2020,"Susilo", 3);
+        Buku10 m2= new Buku10(20212,"Desain UI",2021,"Supriandi", 3);
+        Buku10 m3 = new Buku10(20211,"Web Programming",2022,"Pustaka Adi", 2);
+        Buku10 m4 = new Buku10(20210,"Etika Mahasiswa",2023,"Darmawan Adi", 2);
         data.tambah(m);
-        data.tambah(m4);
+        data.tambah(m1);
         data.tambah(m2);
+        data.tambah(m3);
+        data.tambah(m4);
 
         // System.out.println("==============================");
         // System.out.println("Data keseluruhan buku: ");
