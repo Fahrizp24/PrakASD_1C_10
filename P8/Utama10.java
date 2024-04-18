1ac496ea83dcad4ad6326e7fbe24a81b1b4443df
Fahrizp24 <Fpradian24@gmail.com>
2024-04-18 08:23:10 +0700

    P8_Percobaan1_pertanyaan4_1C_10

--- P8/Utama10.java
+++ P8/Utama10.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class Utama10 {
     public static void main(String[] args) {
-        Gudang10 gudang = new Gudang10(7);
         Scanner input10 = new Scanner(System.in);
         Scanner inputstr10 = new Scanner(System.in);
+        System.out.print("Masukkan jumlah kapasitas gudang : ");
+        int kapasitas = input10.nextInt();
+        Gudang10 gudang = new Gudang10(kapasitas);
         while (true) {
             System.out.println("\n Menu:");
             System.out.println("1. Tambah Barang");
             System.out.println("2. Ambil Barang");
             System.out.println("3. Tampilkan Tumpukan Barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Lihat Barang Teratas");
+            System.out.println("5. Keluar");
             System.out.print("Pilih Operasi: ");
             int pilihan = input10.nextInt();
             switch (pilihan) {
@@ -33,6 +36,9 @@ public class Utama10 {
                     gudang.tampilkanBarang();
                     break;
                 case 4: 
+                    gudang.lihatBarangTeratas();
+                    break;
+                case 5: 
                     break;
                 default:
                     System.out.println("Pilihan yang anda input tidak valid");
