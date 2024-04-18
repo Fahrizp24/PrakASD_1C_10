62e366fe64a65b39cb84e2fb90a2701464992fab
Fahrizp24 <Fpradian24@gmail.com>
2024-04-18 08:56:44 +0700

    P8_percobaan2_1C_10

--- P8/Gudang10.java
+++ P8/Gudang10.java
@@ -40,6 +40,7 @@ public class Gudang10 {
             Barang10 delete = tumpukan[top];
             top--;
             System.out.println("Barang "+delete.nama+" diambil dari gudang");
+            System.out.println("Kode unik dalam biner: "+konversiDesimalkeBiner(delete.kode));
             return delete;
         }else{
             System.out.println("Tumpukan barang kosong");
@@ -67,5 +68,19 @@ public class Gudang10 {
         } else {
             System.out.println("Tumpukan barang kosong");
         }
+    }
+
+    public String konversiDesimalkeBiner(int kode){
+        StackKonversi10 stack = new StackKonversi10();
+        while (kode>0) {
+            int sisa = kode % 2;
+            stack.push(sisa);
+            kode = kode / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner+=stack.pop();
+        }
+        return biner;
     }
 }
