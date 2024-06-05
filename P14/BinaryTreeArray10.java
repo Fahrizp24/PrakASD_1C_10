cfbf49e87bd54dd55014b19f1dfc39d79f98b1a6
Fahrizp24 <Fpradian24@gmail.com>
2024-06-06 00:45:27 +0700

    P14_Tugas5_1C-10

--- P14/BinaryTreeArray10.java
+++ P14/BinaryTreeArray10.java
@@ -20,4 +20,42 @@ public class BinaryTreeArray10 {
             TraverseInOrder(2*idxStart+2);
         }
     }
+
+    void TraversePrOrder(int idxStart){
+        if (idxStart<=idxLast) {
+            System.out.print(data[idxStart]+ " ");
+            TraversePrOrder(2*idxStart+1);
+            TraversePrOrder(2*idxStart+2);
+        }
+    }
+
+    void TraversePostOrder(int idxStart){
+        if (idxStart<=idxLast) {
+            TraversePostOrder(2*idxStart+1);
+            TraversePostOrder(2*idxStart+2);    
+            System.out.print(data[idxStart]+ " ");
+        }
+        
+    }
+
+    void add(int input){
+        int idxStart = 0;
+        int idx=0;
+        while (idx<data.length-1) {
+            if (data[idx]==0) {
+                data[idx]=input;
+                if (idxLast<idx) {
+                    idxLast=idx;
+                }
+                return;
+            } else if (input<data[idxStart]) {
+                idx= idx*2+1;
+            } else if (input > data[idxStart]) {
+                idx=idx*2+2;
+            }else if (input==data[idx]){
+                System.out.println("Data sudah ada");
+                return;
+            }
+        }            
+    }
 }
--- P14/BinaryTreeArrayMain10.java
+++ P14/BinaryTreeArrayMain10.java
@@ -6,8 +6,16 @@ public class BinaryTreeArrayMain10 {
         int[] data = {6,4,8,3,5,7,9,0,0,0};
         int idxLast = 6;
         bta.populateData(data, idxLast);
-        System.out.print("\n InOrder Traversal : ");
+        System.out.print("\nInOrder Traversal : ");
         bta.TraverseInOrder(0);
         System.out.println("\n");
+        System.out.println("Menambahkan data 2");
+        bta.add(2);
+        System.out.print("\nInorder Traversal : ");
+        bta.TraverseInOrder(0);
+        System.out.print("\nPreorder Traversal : ");
+        bta.TraversePrOrder(0);
+        System.out.print("\nPostorder Traversal : ");
+        bta.TraversePostOrder(0);
     }
 }
