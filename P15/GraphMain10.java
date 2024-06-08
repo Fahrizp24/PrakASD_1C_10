d84add865855a07f99d09a52d0181c8fffea8c78
Fahrizp24 <Fpradian24@gmail.com>
2024-06-08 22:58:53 +0700

    P15_Percobaan2_Pertanyaan4_1C_10

--- P15/GraphMain10.java
+++ P15/GraphMain10.java
@@ -6,12 +6,14 @@ public class GraphMain10 {
     public static void main(String[] args)throws Exception {
         Scanner input  = new Scanner(System.in);
         Graph10 gedung = new Graph10(6);
+        GraphMatriks10 gdg = new GraphMatriks10(4);
         gedung.addEdge(0, 1, 50);
         gedung.addEdge(0, 2, 100);
         gedung.addEdge(1, 3, 70);
         gedung.addEdge(2, 3, 40);
         gedung.addEdge(3, 4, 60);
         gedung.addEdge(4, 5, 80);
+
         // gedung.degree(0);
         // gedung.printGraph();
         // gedung.removeEdge(1, 3);
@@ -27,7 +29,6 @@ public class GraphMain10 {
         //     System.out.println("Gedung "+(char) ('A'+asal)+" dan "+(char)('A'+tujuan)+" Tidak Bertetangga");
         // }
 
-        GraphMatriks10 gdg = new GraphMatriks10(4);
         gdg.makeEdge(0, 1, 50);
         gdg.makeEdge(1, 0, 60);
         gdg.makeEdge(1, 2, 70);
@@ -38,5 +39,9 @@ public class GraphMain10 {
         System.out.println("Hasil Setelah Penghapusan Edge");
         gdg.removeEdge(2, 1);
         gdg.printGraph();
+        System.out.print("Masukkan gedung untuk dihitung degreenya: ");
+        int degree = input.nextInt();
+        gdg.printDegree(degree);
+        
     }
 }
--- P15/GraphMatriks10.java
+++ P15/GraphMatriks10.java
@@ -24,10 +24,25 @@ public class GraphMatriks10 {
                 if (matriks[i][j]!=-1) {
                     System.out.print("Gedung"+(char) ('A'+j)+"( "+ matriks[i][j]+" m),");
                 }
-                
+
             }
             System.out.println();
         }
+    }
+
+    public void printDegree(int asal){
+        int degree [][]=new int[vertex][2];
+        for (int i = 0; i < vertex; i++) {
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j]>0) {
+                    degree[i][0]++;
+                    degree[j][1]++;
+                }
+            }
+        }
+        System.out.println("InDegree dari Gedung "+(char) ('A'+asal)+": "+degree[asal][0]);
+        System.out.println("OutDegree dari Gedung "+(char) ('A'+asal)+": "+degree[asal][1]);
+        System.out.println("Degree dari Gedung "+(char) ('A'+asal)+": "+(degree[asal][0]+degree[asal][1]));        
     }
 
 }
